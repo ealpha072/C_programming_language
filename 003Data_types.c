0d5efd8fd0741028a2f28190a46aef8aea4d81ee
ealpha072 <ealpha072@gmail.com>
2023-02-23 19:48:56 +0000

    Data types

---
+++ 003Data_types.c
@@ -0,0 +1,14 @@
+//primary {int, float, char, void} and secondary data types {struct, union, enum
+// typedef}.
+//Primary further classified as short, long,double, long double, 
+//signed and unsigned data types 
+//shot and long are used to dictate amount of memory space to allocate 
+//short int allocates 2bytes and long int 4bytes
+//signed accounts for both positive and negative nums, unsigned only + from 0 to
+//65535
+//
+//long and long double arre used to increase range of values represented by floats
+//Chars - only holds single characters. 1 byte, 8bits, 256(2^8) chars in C
+
+char ch = 'qw' //invalid char assignment
+//
