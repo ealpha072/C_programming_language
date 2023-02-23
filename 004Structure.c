4504f3944a021e98a395c1d4c9f12960aa44f17d
ealpha072 <ealpha072@gmail.com>
2023-02-23 20:57:57 +0000

    Tasks

---
+++ 004Structure.c
@@ -0,0 +1,12 @@
+//The structure of a C program can be mainly divided into six parts, 
+//each having its purpose. It makes the program easy to read, easy to 
+//modify, easy to document, and makes it consistent in format.
+//
+//1. Documentation 
+//2. Link - All header files 
+//3. Definition - Includes preprocessor directive which conatines symbolic
+//constants
+//4. Global Declatations - Includes declaration of global variables, 
+//function declarations, static global variables, and functions.
+//5. main () function
+//6. Sub programs
